0c02b46afea760472aab54b6a3cd5de12f8c69bd
Paula Espinosa <paulawork@hotmail.com>
2021-05-25 12:46:26 -0300

    java

---
+++ variables.js
@@ -0,0 +1,29 @@
+// Variables
+
+var nombre = "Paula";
+
+console.log (nombre);
+
+var numero = 1000;
+console.log(numero);
+
+// variable tipo objeto
+var persona = {
+    nombre: "Nico",
+    apellido: "Perez",
+    telefono: 12345678,
+    
+};
+
+console.log(persona);
+
+//trabajando con el DOM
+
+let titulo = document.getElementById('titulo');
+
+//escribo en el archivo html
+
+titulo.innerHTML = "Nuevo título para el index del html";
+
+let parrafo = document.getElementById('parrafo');
+parrafo.innerHTML = "Nuevo titulo para el párrafo";
